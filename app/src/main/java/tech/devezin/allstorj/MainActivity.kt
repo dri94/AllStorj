2fb1e2bb33f444058facbd0f88cfcc76eacd1bdf
Adrian <2895303+dri94@users.noreply.github.com>
2020-03-30 17:07:14 -0400

    update theming

--- app/src/main/java/tech/devezin/allstorj/MainActivity.kt
+++ app/src/main/java/tech/devezin/allstorj/MainActivity.kt
@@ -1,7 +1,10 @@
 package tech.devezin.allstorj
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import kotlinx.android.synthetic.main.activity_main.*
 import tech.devezin.allstorj.onboarding.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().replace(R.id.mainContainer, LoginFragment.newInstance()).commit()
+            setFragmentContainerBottomMargin()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.mainContainer, LoginFragment.newInstance()).commit()
+
+        }
+    }
+
+    private fun setFragmentContainerBottomMargin() {
+        mainBottomNavigation.post {
+            val height = mainBottomNavigation.height
+            val params: CoordinatorLayout.LayoutParams =
+                mainContainer.layoutParams as CoordinatorLayout.LayoutParams
+            params.setMargins(0, 0, 0, height)
+            mainContainer.layoutParams = params
         }
     }
 }

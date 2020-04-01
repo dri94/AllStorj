090a46f3f246da5e4b7aa76c552fc2dbb755831b
Adrian <2895303+dri94@users.noreply.github.com>
2020-04-01 09:19:04 -0400

    enable files source to accept options

--- app/src/main/java/tech/devezin/allstorj/data/sources/FilesSource.kt
+++ app/src/main/java/tech/devezin/allstorj/data/sources/FilesSource.kt
@@ -1,18 +1,21 @@
 package tech.devezin.allstorj.data.sources
 
 import io.storj.Bucket
+import io.storj.ObjectInfo
+import io.storj.ObjectUploadOption
 import io.storj.StorjException
 import java.io.InputStream
 
 class FilesSource : BaseSource() {
 
-    fun createFile(
+    fun uploadFile(
         bucket: Bucket,
         filePath: String,
-        inputStream: InputStream
+        inputStream: InputStream,
+        vararg options: ObjectUploadOption
     ): Result<Unit, Exception> {
         return try {
-            Success(bucket.uploadObject(filePath, inputStream))
+            Success(bucket.uploadObject(filePath, inputStream, *options))
         } catch (ex: StorjException) {
             Error(ex)
         }
